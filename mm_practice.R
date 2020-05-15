f34addd44505e93c73eee547ad4215b00b00aa21
sippstress <8432003+rsippy@users.noreply.github.com>
2020-05-15 12:45:14 -0500

    initial commit

---
+++ mm_practice.R
@@ -0,0 +1,9 @@
+library(lme4)
+library(MASS)
+library(car)
+library(pbkrtest)
+library(RLRsim)
+library(ggplot2)
+library(pbnm)
+
+install.packages("path/pbnm_0.3.0.9001.tar.gz", repos=NULL,type="source")
